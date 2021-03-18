11898222d4ec4951f6fb0c6fbc8afeaa37891f1f
julien <julien@papier-code.fr>
2021-03-18 16:38:46 +0100

    mise à jour wpréform

--- include/woocommerce/templates/woo-template-product-resum.php
+++ include/woocommerce/templates/woo-template-product-resum.php
@@ -117,7 +117,7 @@ function pc_woo_display_product_resum_content( $custom_product, $hn = 2 ) {
 	}
 	$img_datas = pc_get_post_resum_img_datas( $product_id, $product_title, $product_metas );
 	// description
-	$product_desc = pc_get_post_resum_excerpt( $product_id, $product_metas );
+	$product_desc = pc_get_post_resum_description( $product_id, $product_metas );
 
 
 	/*----------  Affichage  ----------*/		
