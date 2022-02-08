be774a75631a793021373e890367594dd9204029
papiercode <julien@papier-code.fr>
2022-02-08 08:55:41 +0100

    25

--- scripts/pc-project.js
+++ scripts/pc-project.js
@@ -72,30 +72,34 @@ var $st_list = $('.st-list');
 
 if ( $st_list.length > 0 ) {
 
-	var $st_items = $st_list.find('.st'),
-	$st_items_fake = 0;
+	$st_list.each( function() { 
 
-	switch ( $st_items.length ) {
-		case 1 :
-		case 4 :
-			$st_items_fake = 2;
-			break;
-		case 2 :
-		case 3 :
-		case 5 :
-			$st_items_fake = 1;
-			break;
-	}
+		var $st_items = $(this).find('.st'),
+		$st_items_fake = 0;
 
-	if ( $html.hasClass('is-home') && $st_items.length == 4 ) { $st_items_fake = 0; }
+		switch ( $st_items.length ) {
+			case 1 :
+			case 4 :
+				$st_items_fake = 2;
+				break;
+			case 2 :
+			case 3 :
+			case 5 :
+				$st_items_fake = 1;
+				break;
+		}
 
-	for ( i = 0; i < $st_items_fake; i++) {
-		
-		var tag = $st_items.eq(0).prop('tagName').toLowerCase();
+		if ( $html.hasClass('is-home') && $st_items.length == 4 ) { $st_items_fake = 0; }
 
-		$st_list.append('<'+tag+' class="st--fake '+$st_items.eq(0).attr('class')+'" aria-hidden="true"></'+tag+'>');
+		for ( i = 0; i < $st_items_fake; i++) {
+			
+			var tag = $st_items.eq(0).prop('tagName').toLowerCase();
 
-	}
+			$(this).append('<'+tag+' class="st--fake '+$st_items.eq(0).attr('class')+'" aria-hidden="true"></'+tag+'>');
+
+		}
+
+	});
 
 }
 
